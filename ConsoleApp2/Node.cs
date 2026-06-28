0b5e04dd2c22a2e25b7871a183981ae9ce01b1ae
knara7823 <knaraharutyunova@gmail.com>
2026-06-28 20:15:49 +0400

    Implimenting AddFirst, AddLast and PrintNodes funcs

---
+++ ConsoleApp2/Node.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace MyApp;
+
+public class Node<T>
+{
+    private T? value;
+
+    public Node<T> next{set;get;}
+
+    public Node<T> prev{set;get;}
+
+    public Node(T? value)
+    {
+        this.value = value;
+    }
+
+    public T GetValue() =>  this.value;
+    
+}
+
+public class LinkedList<T>
+{
+    private Node<T>? head;
+    private Node<T>? tail;
+
+    public LinkedList()
+    {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void AddLast(T value)
+    {
+        
+        if (head == null)
+        {
+            head = new Node<T>(value);
+            tail = head;
+            return;
+        }
+        //4 5
+        //5 previoous 4
+        //4 next 5
+        Node<T> node = new Node<T>(value);
+        node.prev = tail;
+        tail!.next = node;
+        tail = node;
+        
+    }
+
+    public void AddFirst(T value)
+    {
+        if (head == null)
+        {
+            head = new Node<T>(value);
+            tail = head;
+            return;
+        }
+        // 4
+        // 5 4
+        Node<T> node = new Node<T>(value);
+        node.next = head;
+        head.prev = node;
+        head = node;
+    }
+
+    public void PrintNodes()
+    {
+        Node<T>? currentNode = this.head;
+
+        Console.WriteLine(currentNode!.GetValue());
+
+        while (currentNode.next != null)
+        {
+            currentNode = currentNode.next;
+            Console.WriteLine(currentNode.GetValue());
+        }
+      
+    }
+    
+    
+  
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace MyApp
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(85);
+            linkedList.AddLast(33);
+            linkedList.AddFirst(333);
+            linkedList.AddLast(34133);
+            linkedList.AddFirst(0);
+            linkedList.AddFirst(4);
+            linkedList.PrintNodes();
+
+        }
+
+    }
+}
